291ceaadcbc357a67c4f3c8c3a328c075656e6c8
Tres Tarwater <trest03@gmail.com>
2020-06-02 03:11:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,8 +41,14 @@ def position_taken?(board, index)
 end
 end
 
-loop do
-def turn(board)
-   puts "Please enter 1-9:"
-   
- end
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
